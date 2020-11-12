8bf9c59fa10e95c47663dcfeb44182868f5d0f4e
Frankie Roberto <frankie@frankieroberto.com>
2020-11-12 12:36:19 +0000

    Make sure that the open attribute doesn't have a value
    The Rails tag builder displays boolean attributes in the format of `open="open"` – this doesn't match the HTML of the govuk-frontend output, which just uses `open`.
    
    To work around this, the code from ActionView for outputting html attributes has been forked.

--- lib/govuk-frontend/details.rb
+++ lib/govuk-frontend/details.rb
@@ -15,24 +15,60 @@ module GovukFrontend
 
     attributes.merge!(params["attributes"]) if params["attributes"]
 
-    tag.details(attributes.symbolize_keys) do
-      "\n  ".html_safe +
-      tag.summary(class: "govuk-details__summary") do
-        "\n    ".html_safe +
-        tag.span(class: "govuk-details__summary-text") do
-          "\n      ".html_safe +
-          (params["summaryHtml"] ? params["summaryHtml"].html_safe : params["summaryText"]) +
-          "\n    ".html_safe
-        end +
-        "\n  ".html_safe
+    "<details #{tag_options_with_booleans_having_no_value(attributes)}> ".html_safe +
+    tag.summary(class: "govuk-details__summary") do
+      " ".html_safe +
+      tag.span(class: "govuk-details__summary-text") do
+        " ".html_safe +
+        (params["summaryHtml"] ? params["summaryHtml"].html_safe : params["summaryText"]) +
+        " ".html_safe
       end +
-      "\n  ".html_safe +
-      tag.div(class: "govuk-details__text") do
-        "\n    ".html_safe +
-        (params["html"] ? params["html"].html_safe : params["text"]) +
-        "\n  ".html_safe
-      end +
-      "\n".html_safe
-    end
+      " ".html_safe
+    end +
+    " ".html_safe +
+    tag.div(class: "govuk-details__text") do
+      " ".html_safe +
+      (params["html"] ? params["html"].html_safe : params["text"]) +
+      " ".html_safe
+    end +
+    " </details>".html_safe
   end
+
+  private
+
+  # Simplified fork of method from ActionView::Helpers::TagHelper
+  #
+  # The main difference is that the "open" attribute is outputted with no
+  # value, eg <details open> rather than with the same value as the attribute
+  # name, eg <details open="open"> - this is to match the output from the
+  # govuk-frontend component.
+  def tag_options_with_booleans_having_no_value(options, escape = true)
+    return if options.blank?
+    output = +""
+    sep    = " "
+    options.each_pair do |key, value|
+      if key == "open"
+        output << sep
+        output << key
+      else
+        output << sep
+        output << tag_option(key, value, escape)
+      end
+    end
+    output unless output.empty?
+  end
+
+  # Copied from ActionView::Helpers::TagHelper
+  def tag_option(key, value, escape)
+    case value
+    when Array, Hash
+      value = TagHelper.build_tag_values(value) if key.to_s == "class"
+      value = escape ? safe_join(value, " ") : value.join(" ")
+    else
+      value = escape ? ERB::Util.unwrapped_html_escape(value) : value.to_s
+    end
+    value = value.gsub('"', "&quot;") if value.include?('"')
+    %(#{key}="#{value}")
+  end
+
 end
